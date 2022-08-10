c596c0d221df886dd8675891f872c1ca4a67b901
YKJ <rudwns7552@gmail.com>
2022-08-11 08:37:34 +0900

    1. 동시성 문제를 해결하기 위해 ThreadLocal 적용    >> ThreadLocal은 쓰레드 마다 메모리를 공유하지 않고 각각 할당    >> 단, 모든 요청이 끝난 후 생성된 ThreadLocal을 remove 해주어야 함       그렇지 않으면 WAS 쓰레드풀에서 remove처리 되지 않은 ThreadLocal이 다른 사용자에게 조회 되는 심각한 문제 발생

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package hello.advanced;
 
-import hello.advanced.trace.logtrace.FiledLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +9,6 @@ import org.springframework.context.annotation.Configuration;
 public class LogTraceConfig {
     @Bean
     public LogTrace logTrace(){
-        return new FiledLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
---
+++ src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,42 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+    private ThreadLocalService  threadLocalService  = new ThreadLocalService();
+
+    @Test
+    void thread_local_test(){
+        log.info("main satrt");
+        Runnable userA = () ->{
+            threadLocalService.logic("userA");
+        };
+
+        Runnable userB = () ->{
+            threadLocalService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000);//동시성 문제 발생 X
+        sleep(100);//동시성 문제 발생 O
+        threadB.start();
+        sleep(3000);//메인 쓰레드 종료 대기
+        log.info("main exit");
+    }
+
+    private void sleep(int milis) {
+        try {
+            Thread.sleep(milis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
